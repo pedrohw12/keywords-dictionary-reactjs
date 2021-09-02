d735115194c4cdc64a0bf4b85cc38f06b6597c75
Pedro Wanderley <pedrowanderley@mini-de-pedro.localdomain>
2021-09-01 21:42:09 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const keywords = ["function", "const", "let", "export"];
 
 function App() {
+  const [wordsToShow, setWordsToShow] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  function calculatePercent(score) {
+    const totalScore = keywords.length;
+    const fraction = score / totalScore;
+    return fraction * 100;
+  }
+
+  function replay() {
+    setWordsToShow([]);
+  }
+
+  function handleChange(value) {
+    setInputValue(value);
+
+    if (keywords.indexOf(value) !== -1) {
+      setInputValue("");
+      setWordsToShow([...wordsToShow, value]);
+    } else {
+      return;
+    }
+  }
+
+  function handleGiveUp() {
+    setWordsToShow(keywords);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>5:00</p>
+      <button onClick={handleGiveUp}>Desistir</button>
+      <br />
+      <br />
+      <p>
+        Score: {wordsToShow.length}/{keywords.length} <br />
+        Percentual: Você fez {calculatePercent(wordsToShow.length)}%
+      </p>
+      <button onClick={replay}>Replay</button>
+      <input
+        value={inputValue}
+        placeholder="palavra-chave"
+        onChange={(e) => handleChange(e.target.value)}
+      />
+      {keywords.map((item, index) => {
+        if (wordsToShow.indexOf(item) !== -1) {
+          return <p key={index}>{wordsToShow[wordsToShow.indexOf(item)]}</p>;
+        } else {
+          return <p key={index}>______</p>;
+        }
+      })}
     </div>
   );
 }
